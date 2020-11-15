d4586fb27cce2f14b69bb441f8a373290e01f57f
SzaboZoltan1998 <71753869+SzaboZoltan1998@users.noreply.github.com>
2020-11-15 20:27:19 +0200

    Character

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,53 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Player_Movement : MonoBehaviour
+{
+    //Components
+
+    Rigidbody myRigidbody;
+    Transform myAvatar;
+
+    //Player movement
+
+    [SerializeField] InputAction WASD;
+    Vector2 movementInput;
+    [SerializeField] float movementSpeed;
+
+    private void OnEnable()
+    {
+        WASD.Enable();
+    }
+
+    private void OnDisable()
+    {
+        WASD.Disable();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myAvatar = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput = WASD.ReadValue<Vector2>();
+
+        if (movementInput.x != 0)
+        {
+            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
+        }
+    }
+    private void FixUpdate()
+    {
+        myRigidbody.velocity = movementInput * movementSpeed;
+    }
+}*/
